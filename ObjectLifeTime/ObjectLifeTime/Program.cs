a53a013d0bc8c81dc04c2d7ddefa3afe1f9ecc9d
leonischu <nischal.201423@ncit.edu.np>
2025-12-05 11:08:30 +0545

    Understanding of scope

--- ObjectLifeTime/ObjectLifeTime/Program.cs
+++ ObjectLifeTime/ObjectLifeTime/Program.cs
@@ -1,75 +1,145 @@
-﻿using System;
+﻿/*
+using System;
 namespace ObjectLifeTime
+{
+   class Program
+   {
+       static void Main(string[] args) {
+
+           Car.MyMethod();
+
+    Car myCar = new Car();
+
+       myCar.Make = "oldmobile";
+       myCar.Model = "cutlas supreme";
+       myCar.Year = 1986;
+       myCar.Color = "silver";
+
+    Car myOtherCar;
+    myOtherCar = myCar;
+
+    //Console.WriteLine($"{myOtherCar.Make},{myOtherCar.Model},{myOtherCar.Year},{myOtherCar.Color}");
+
+    myOtherCar.Model = "98";
+    Console.WriteLine($"{myCar.Make},{myCar.Model},{myCar.Year},{myCar.Color}");
+
+    myOtherCar = null;
+
+    myCar = null;
+
+    Car myThirdCar = new Car("Ford", "Escape", 2005, "White");
+    Console.WriteLine($"{myThirdCar.Make},{myThirdCar.Model},{myThirdCar.Year},{myThirdCar.Color}");
+
+
+
+           Console.ReadLine();
+
+
+
+       }
+
+   }
+   class Car
+   {
+       public string Make { get; set; }
+       public string Model { get; set; }
+       public int Year { get; set; }
+       public string Color { get; set; }
+
+
+       public Car()
+       {
+
+
+           this.Make = "Nissan";
+       }
+       public Car(string make, string model, int year, string color)
+       {
+           Make = make;
+           Model = model;
+           Year = year;
+           Color = color;
+
+       }
+       public static void myMethod()
+       {
+           Console.WriteLine("Called the static MyMethod");
+       }
+
+
+   public static void MyMethod()
+       {
+           Console.WriteLine("Called the static MyMethod");
+       }
+
+   }
+}
+ */
+
+using System;
+
+namespace UnderstandingScope
 {
     class Program
     {
-        static void Main(string[] args) {
 
-            Car.MyMethod();
-            /*
-     Car myCar = new Car();
-
-        myCar.Make = "oldmobile";
-        myCar.Model = "cutlas supreme";
-        myCar.Year = 1986;
-        myCar.Color = "silver";
-
-     Car myOtherCar;
-     myOtherCar = myCar;
-
-     //Console.WriteLine($"{myOtherCar.Make},{myOtherCar.Model},{myOtherCar.Year},{myOtherCar.Color}");
-
-     myOtherCar.Model = "98";
-     Console.WriteLine($"{myCar.Make},{myCar.Model},{myCar.Year},{myCar.Color}");
-
-     myOtherCar = null;
-
-     myCar = null;
-
-     Car myThirdCar = new Car("Ford", "Escape", 2005, "White");
-     Console.WriteLine($"{myThirdCar.Make},{myThirdCar.Model},{myThirdCar.Year},{myThirdCar.Color}");
-    */
+        private static string k = "";
 
 
-            Console.ReadLine();
+        static void Main(string[] args)
+        {
+
+           
+            string j = "";
+            for (int i = 0; i < 10; i++)
+            {
+                j= i.ToString();    
+                Console.WriteLine(i);
+                k=i.ToString();
+
+                if (i == 9)
+                {
+                    string l = i.ToString();
+
+                }
 
 
+
+            }
+            Console.WriteLine("Outside of the for: " + j + k);
+            HelperMethod();
+
+            Car myCar= new Car();
+            myCar.DoSomething(); 
+
+            Console.ReadLine(); 
 
         }
 
+    static void HelperMethod()
+        {
+            Console.WriteLine("Value of k from helper mwthod " + k);
+        }
+
     }
-    class Car
+
+    class Car 
     {
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-        public string Color { get; set; }
-
-
-        public Car()
+    public void DoSomething()
         {
-
-
-            this.Make = "Nissan";
+            Console.WriteLine(helperMethod());
         }
-        public Car(string make, string model, int year, string color)
+        private string helperMethod()
         {
-            Make = make;
-            Model = model;
-            Year = year;
-            Color = color;
-
-        }
-        public static void myMethod()
-        {
-            Console.WriteLine("Called the static MyMethod");
+            return "Hello world";
         }
     
-    
-    public static void MyMethod()
-        {
-            Console.WriteLine("Called the static MyMethod");
-        }
-
     }
+
+
+
+
+
+
+
 }
